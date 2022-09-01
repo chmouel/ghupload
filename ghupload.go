4feaa8277b827e7cbebbe550e1da15d3b0e13f92
Chmouel Boudjnah <chmouel@redhat.com>
2022-09-01 22:42:21 +0200

    Third
    Signed-off-by: Chmouel Boudjnah <chmouel@redhat.com>

--- ghupload.go
+++ ghupload.go
@@ -6,7 +6,9 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/google/go-github/v47/github"
 	"github.com/urfave/cli/v2"
@@ -47,62 +49,91 @@ func (g *GH) createClient(ctx context.Context) {
 	g.client = github.NewClient(tc)
 }
 
-func (g *GH) upload(ctx context.Context, src, dst, commitMessage string) error {
-	blob, err := parsePath(dst)
+func (g *GH) upload(ctx context.Context, args []string, commitMessage string) error {
+	if commitMessage == "" {
+		commitMessage = "Uploaded by ghupload"
+	}
+	// dst is the last argument
+	dst := args[len(args)-1]
+	dstBlob, err := parsePath(dst)
 	if err != nil {
 		return err
 	}
-	file, err := os.Open(src)
-	if err != nil {
-		return err
+	isDirUpload := strings.HasSuffix(dstBlob.path, "/")
+
+	// srcs are everything but the last argument
+	srcs := args[:len(args)-1]
+
+	// if dst don't finish by a / and we have multiple srcs then error out
+	if !isDirUpload && len(srcs) > 1 {
+		return fmt.Errorf("dst path must end with a / if you want to upload multiple files")
 	}
-	// read file
-	content, err := io.ReadAll(file)
-	if err != nil {
-		return err
+	entries := []*github.TreeEntry{}
+	for _, src := range srcs {
+		fi, err := os.Stat(src)
+		if err != nil {
+			return err
+		}
+		// if it's a normal file then perm == 100644
+		perm := "100644"
+		if fi.IsDir() {
+			perm = "100644"
+		} else if fi.Mode()&os.ModeSymlink != 0 {
+			return fmt.Errorf("symlink is not supported (cause i am lazy)")
+		}
+
+		file, err := os.Open(src)
+		if err != nil {
+			return err
+		}
+		content, err := io.ReadAll(file)
+		if err != nil {
+			return err
+		}
+		entries = append(entries, &github.TreeEntry{
+			Path:    github.String(filepath.Join(dstBlob.path, fi.Name())),
+			Mode:    github.String(perm),
+			Type:    github.String("commit"),
+			Content: github.String(string(content)),
+		})
+
 	}
 
-	branch := blob.branch
+	branch := dstBlob.branch
+	repoInfo, _, err := g.client.Repositories.Get(ctx, dstBlob.owner, dstBlob.repo)
+	if err != nil {
+		return err
+	}
 	if branch == "" {
-		branchInfo, _, err := g.client.Repositories.Get(ctx, blob.owner, blob.repo)
-		if err != nil {
-			return err
-		}
-		branch = branchInfo.GetDefaultBranch()
+		branch = repoInfo.GetDefaultBranch()
 	}
-	fc, dc, _, _ := g.client.Repositories.GetContents(ctx, blob.owner, blob.repo, blob.path, &github.RepositoryContentGetOptions{})
-	if dc != nil {
-		return fmt.Errorf("destination is a directory: %s", dst)
+	branchInfo, _, err := g.client.Repositories.GetBranch(ctx, dstBlob.owner, dstBlob.repo, branch, true)
+	if err != nil {
+		return err
 	}
-	// if file hasn't been uploaded yet, create it
-	if fc == nil {
-		if commitMessage == "" {
-			commitMessage = "created by ghupload"
-		}
-		cntResp, _, err := g.client.Repositories.CreateFile(ctx, blob.owner, blob.repo, blob.path, &github.RepositoryContentFileOptions{
-			Message: github.String(commitMessage),
-			Content: content,
-			Branch:  github.String(branch),
-		})
-		if err != nil {
-			return err
-		}
-		fmt.Printf("File has been uploaded to: %s\n", cntResp.GetContent().GetHTMLURL())
-	} else { // else update the file
-		if commitMessage == "" {
-			commitMessage = "updated by ghupload"
-		}
-		ud, _, err := g.client.Repositories.UpdateFile(ctx, blob.owner, blob.repo, blob.path, &github.RepositoryContentFileOptions{
-			Message: github.String(commitMessage),
-			SHA:     fc.SHA,
-			Content: content,
-		})
-		if err != nil {
-			return err
-		}
-		fmt.Printf("File has been updated on: %s\n", ud.GetContent().GetHTMLURL())
+	lastBranchCommit := branchInfo.GetCommit().GetSHA()
+	tree, _, err := g.client.Git.CreateTree(ctx, dstBlob.owner, dstBlob.repo, lastBranchCommit, entries)
+	if err != nil {
+		return err
 	}
-	return nil
+	// create a commit
+	commit, _, err := g.client.Git.CreateCommit(ctx, dstBlob.owner, dstBlob.repo, &github.Commit{
+		Message: github.String(commitMessage),
+		Tree:    tree,
+		Parents: []*github.Commit{{Tree: tree, SHA: github.String(lastBranchCommit)}},
+		Author:  &github.CommitAuthor{Name: github.String("ghupload"), Email: github.String("ghupload@ghupload.com")},
+	})
+	if err != nil {
+		return err
+	}
+	// update the branch with the commit
+	_, _, err = g.client.Git.UpdateRef(ctx, dstBlob.owner, dstBlob.repo, &github.Reference{
+		Ref:    github.String(fmt.Sprintf("refs/heads/%s", branch)),
+		Object: &github.GitObject{SHA: commit.SHA},
+	}, false)
+	fmt.Printf("commit has been created: %s\n", commit.GetHTMLURL())
+	fmt.Printf("branch %s has been updated to commit %s\n", branch, commit.GetSHA())
+	return err
 }
 
 func newGH(token string) *GH {
@@ -134,15 +165,16 @@ func app() error {
 				Name:  "upload",
 				Flags: commonFlag,
 				Action: func(c *cli.Context) error {
-					src := c.Args().Get(0)
-					dst := c.Args().Get(1)
+					if c.Args().Len() != 2 {
+						return fmt.Errorf("invalid number of arguments we need at least a src and a dst")
+					}
 					token := c.String("token")
 					if token == "" {
 						return cli.Exit("github token need to be set", 1)
 					}
 					g := newGH(token)
 					g.createClient(ctx)
-					return g.upload(ctx, src, dst, c.String("message"))
+					return g.upload(ctx, c.Args().Slice(), c.String("message"))
 				},
 				Usage: "upload a file",
 			},
